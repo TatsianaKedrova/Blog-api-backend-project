39c34cade7b73681580f72a128bb36ee7796b782
TatsianaKedrova <Baletrot@gmail.com>
2023-04-28 20:42:37 +0300

    added export keyword

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 const port = process.env.PORT || 5000;
 
-const startApp = async () => {
+export const startApp = async () => {
   await runDB();
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
