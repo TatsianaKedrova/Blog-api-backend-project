e13d2156f2af65adccf150a14a78aca4f5746846
TatsianaKedrova <Baletrot@gmail.com>
2023-05-15 14:24:28 +0300

    added authValidator; index.d.ts; /me route

--- src/dto/authDTO/authDTO.ts
+++ src/dto/authDTO/authDTO.ts
@@ -9,3 +9,13 @@ export type UsersQueryParams<T> = PagingSortingQueryParams<T> & {
   searchLoginTerm: string | null;
   searchEmailTerm: string | null;
 };
+
+export type MeViewModel = {
+  email: string;
+  login: string;
+  userId: string;
+};
+
+export type LoginSuccessViewModel = {
+  accessToken: string; //jwt token
+};
--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -1,12 +1,16 @@
-import express, { Response } from "express";
+import express, { Request, Response } from "express";
 import { usersService } from "../domain/users-service";
 import { RequestBodyModel } from "../dto/common/RequestModels";
 import { LoginInputModel } from "../dto/authDTO/authDTO";
 import { StatusCodes } from "http-status-codes";
+import { responseErrorValidationMiddleware } from "../middlewares/responseErrorValidationMiddleware";
+import { authValidator } from "../utils/auth-utils/auth-validator";
 export const authRouter = express.Router({});
 
 authRouter.post(
   "/login",
+  authValidator,
+  responseErrorValidationMiddleware,
   async (req: RequestBodyModel<LoginInputModel>, res: Response) => {
     const checkResult = await usersService.checkCredentials(
       req.body.loginOrEmail,
@@ -19,3 +23,5 @@ authRouter.post(
     }
   }
 );
+
+authRouter.get("/me", async (req: Request, res: Response) => {});
