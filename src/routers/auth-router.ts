0e8bc7b2355baa8a190e35f79927c7263dc71005
TatsianaKedrova <Baletrot@gmail.com>
2023-06-16 12:49:31 +0300

    added authValidator to login; added tests for auth

--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -3,10 +3,12 @@ import { usersService } from "../domain/users-service";
 import { RequestBodyModel } from "../dto/common/RequestModels";
 import { LoginInputModel } from "../dto/authDTO/authDTO";
 import { StatusCodes } from "http-status-codes";
+import { authValidator } from "../utils/auth-util/auth-validator";
 export const authRouter = express.Router({});
 
 authRouter.post(
   "/login",
+  authValidator,
   async (req: RequestBodyModel<LoginInputModel>, res: Response) => {
     const checkResult = await usersService.checkCredentials(
       req.body.loginOrEmail,
