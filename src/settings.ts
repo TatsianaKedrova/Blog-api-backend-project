92b6d8fb8d28b70d347ebf94c2df054795f1007a
TatsianaKedrova <Baletrot@gmail.com>
2023-10-11 08:32:07 +0300

    reinstalled @types/cookie-parser

--- src/settings.ts
+++ src/settings.ts
@@ -1,3 +1,4 @@
+import cookieParser from "cookie-parser";
 import express from "express";
 import { videosRouter } from "./routers/video-router";
 import { testingRouter } from "./routers/testing-router";
@@ -6,11 +7,10 @@ import { postsRouter } from "./routers/posts-router";
 import { authRouter } from "./routers/auth-router";
 import { usersRouter } from "./routers/users-router";
 import { commentsRouter } from "./routers/comments-router";
-import cookieParser from "cookie-parser";
 
 export const app = express();
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use("/api/videos", videosRouter);
 app.use("/api/testing", testingRouter);
@@ -18,4 +18,4 @@ app.use("/api/blogs", blogsRouter);
 app.use("/api/posts", postsRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
-app.use("/api/comments", commentsRouter)
+app.use("/api/comments", commentsRouter);
